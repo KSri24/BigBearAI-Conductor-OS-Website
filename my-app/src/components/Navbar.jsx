e5b5acb31f8dc6656b8cf632234b5ef6bb90d0fc
Joshua Cancio <145736028+J-Souffle@users.noreply.github.com>
2024-08-07 13:13:04 -0400

    added starting navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 function Navbar() {
     return(
         <>
-            <p>Test</p>
+            <nav>
+            <a href="#home">Home</a> |
+            <a href="#about">About</a> |
+            <a href="#contact">Contact</a> 
+            </nav>
         </>
     )
 }
