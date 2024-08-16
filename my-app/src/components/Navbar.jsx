08c0598e417bc9005f954f8bec890e0b29eed2e4
SriK <sriramkoduru123@gmail.com>
2024-08-16 09:39:38 -0400

    working navbar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -4,28 +4,28 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className='logo_container'>
-        <div className="navbar_logo">
-          {/* <img
-            className="c-navbar__desktop__logo__img c-navbar__desktop__logo__img--dark"
-            src="https://bigbear.ai/wp-content/themes/bigbear-theme/assets/images/bbai-logo-search.svg"
-            alt="BigBear.ai logo dark version"
-          /> */}
-
-          <img 
-            class="c-navbar__desktop__logo__img c-navbar__desktop__logo__img--light" 
-            src="https://bigbear.ai/wp-content/themes/bigbear-theme/assets/images/Brand-navbar.svg" 
-            alt="BigBear.ai logo dark version"
-          />
-
+      <div className='navbar_content'>
+        <div className='logo_container'>
+          <div className="navbar_logo">
+            <img
+              className="c-navbar__desktop__logo__img c-navbar__desktop__logo__img--dark"
+              src="https://bigbear.ai/wp-content/themes/bigbear-theme/assets/images/bbai-logo-search.svg"
+              alt="BigBear.ai logo dark version"
+            />
+            <img 
+              className="c-navbar__desktop__logo__img c-navbar__desktop__logo__img--light" 
+              src="https://bigbear.ai/wp-content/themes/bigbear-theme/assets/images/Brand-navbar.svg" 
+              alt="BigBear.ai logo light version"
+            />
+          </div>
         </div>
+      
+        <ul className="c-navbar__desktop__navigation__menu__parent">
+          <li><a href="#overview">Overview</a></li>
+          <li><a href="#diagram">Diagram Section</a></li>
+          <li><a href="#contact-us">Contact Us</a></li>
+        </ul>
       </div>
-      <ul className="navbar_list">
-        <li><a href="/">Overview</a></li>
-        <li><a href="/about">Diagram</a></li>
-        <li><a href="/services">About Us</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
     </nav>
   );
 }
