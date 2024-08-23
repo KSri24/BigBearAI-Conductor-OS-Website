c23000d6a2c7998b68d80e6a963f8bfceb7cf079
SriK <sriramkoduru123@gmail.com>
2024-08-23 10:59:18 -0400

    hover functionality complete

--- src/components/diagram.js
+++ src/components/diagram.js
@@ -41,21 +41,54 @@ const diagram = () => {
   return (
     <div className="diagram">
       <Section className="diagram_section-1">
-
         <h2 className="dHeading">Interactive Diagram</h2>
         <img className="diagramImage" src={image} width="900" height="730" alt="Diagram" />
 
-        <div className="circle"></div>
-            <div className="overlay">
-                <div className="overlay-text">
-                    <h2>Hover Text</h2>
-                    <p>This is some dummy text that appears when you hover over the circle. You can add more text here.</p>
-                </div>
+        {/* Circle 1 */}
+        <div className="circle circle-1"></div>
+        <div className="overlay">
+          <div className="overlay-text">
+            <h2>Hover Text 1</h2>
+            <p>This is some dummy text that appears when you hover over circle 1. You can add more text here.</p>
+          </div>
         </div>
 
+        {/* Circle 2 */}
+        <div className="circle circle-2"></div>
+        <div className="overlay">
+          <div className="overlay-text">
+            <h2>Hover Text 2</h2>
+            <p>This is some dummy text that appears when you hover over circle 2. You can add more text here.</p>
+          </div>
+        </div>
+
+        {/* Circle 3 */}
+        <div className="circle circle-3"></div>
+        <div className="overlay">
+          <div className="overlay-text">
+            <h2>Hover Text 3</h2>
+            <p>This is some dummy text that appears when you hover over circle 3. You can add more text here.</p>
+          </div>
+        </div>
+
+        {/* Circle 4 */}
+        <div className="circle circle-4"></div>
+        <div className="overlay">
+          <div className="overlay-text">
+            <h2>Hover Text 4</h2>
+            <p>This is some dummy text that appears when you hover over circle 4. You can add more text here.</p>
+          </div>
+        </div>
+
+        {/* Circle 5 */}
+        <div className="circle circle-5"></div>
+        <div className="overlay">
+          <div className="overlay-text">
+            <h2>Hover Text 5</h2>
+            <p>This is some dummy text that appears when you hover over circle 5. You can add more text here.</p>
+          </div>
+        </div>
       </Section>
-
-
     </div>
   );
 };
